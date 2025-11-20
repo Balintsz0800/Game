0afca348a5137de75883a9b8b783b49e8f9ecf7d
Balintsz0800 <balint0800@gmail.com>
2025-11-20 20:06:58 +0100

    Create Timenight.cs

---
+++ Assets/Scripts/Timenight.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class TimeNight : MonoBehaviour
+{
+    [SerializeField] private float timer; 
+    public bool isDay = true;
+    public TMP_Text text;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text.text = timer.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 60f)
+        {
+            isDay = !isDay;
+        }
+        timer += Time.deltaTime;
+        int minutes = (int)timer / 60;
+        int seconds = (int)timer % 60;
+        text.text = minutes + ":" + seconds.ToString("00");
+    }
+}
